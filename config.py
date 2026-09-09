503bd5c6bbc8d6ee883b7ee9e7663476823b416a
ggggsw112 <lacsoneugine485@gmail.com>
2026-09-09 21:28:01 +0800

    Initial selfbot setup with 24/7 VC and Spotify status

---
+++ config.py
@@ -0,0 +1,14 @@
+# Discord Selfbot Configuration
+
+# Your Discord user token (from DevTools)
+DISCORD_TOKEN = "YOUR_TOKEN_HERE"
+
+# Voice Channel ID to stay in 24/7
+VOICE_CHANNEL_ID = 0  # Replace with actual channel ID
+
+# Spotify playlist/track info (will be displayed as listening status)
+SPOTIFY_TRACK = "Your Song Name"
+SPOTIFY_ARTIST = "Artist Name"
+
+# Auto-deafen settings
+AUTO_DEAFEN = True  # Set to False if you want mic on by default
